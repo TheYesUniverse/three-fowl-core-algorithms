caaaa83688dfd617a75091af76c09afb99bcdad8
mikeadossi <mikeadossi@gmail.com>
2017-01-12 14:58:18 -0800

    work so far

---
+++ test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import isPalindrome from '../src/isPalindrome'
+
+describe('isPalindrome', function(){
+  context('when given "radar"', function(){
+    it('should return true', function(){
+      expect(isPalindrome('radar')).to.equal(true)
+    })
+  })
+  context('when given "bananas"', function(){
+    it('should return false', function(){
+      expect(isPalindrome('bananas')).to.equal(false)
+    })
+  })
+  it('should ignore special characters', function(){
+    expect(isPalindrome('radar')).to.equal(true)
+    expect(isPalindrome('rad%ar')).to.equal(true)
+    expect(isPalindrome('ra/dar')).to.equal(true)
+    expect(isPalindrome('ra/d.....ar')).to.equal(true)
+  })
+  context('when given "A man, a plan, a canal: Panama"', function(){
+    it('should return true', function(){
+      expect(isPalindrome('A man, a plan, a canal: Panama')).to.equal(true)
+    })
+  })
+})
