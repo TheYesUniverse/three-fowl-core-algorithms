a41db3a408ae25883943e11cc5648a7fe2709167
mikeadossi <mikeadossi@gmail.com>
2017-01-12 23:23:01 -0800

    added links to functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
 "use strict"
 
-console.log('js works')
+const welcomePage = () => {
+  console.log('welcomePage button works')
+}
+
+const makeChangePage = () => {
+  console.log('makeChange button works!')
+}
+
+const fizzBuzzPage = () => {
+  console.log('fizzbuzz button works')
+}
+const palindromePage = () => {
+  console.log('palindrome button works')
+}
+const factorialPage = () => {}
+const fibonacciPage = () => {}
+const collatzConjecturePage = () => {}
+const setUnionPage = () => {}
+const setIntersectionPage = () => {}
+const setComplimentPage = () => {}
+const setSymmetricDifferencePage = () => {}
+const mergeSortPage = () => {}
+const bubbleSortPage = () => {}
+const binarySearchPage = () => {}
+const closestPairPage = () => {}
+const connectedGraphPage = () => {}
