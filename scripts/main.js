e3a222a6cc1093ebc54fcce1cd3e43ef65ed5295
mikeadossi <mikeadossi@gmail.com>
2017-01-13 01:15:44 -0800

    routing code written and working

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,45 @@
 "use strict"
 
-const welcomePage = () => {
-  console.log('welcomePage button works')
+const routesObj = {
+  1: '#welcomePage',
+  2: '#makeChangePage',
+  3: '#fizzBuzzPage',
+  4: '#palindromePage',
+  5: '#factorialPage',
+  6:'#fibonacciPage',
+  7:'#collatzConjecturePage',
+  8: '#setUnionPage',
+  9: '#setIntersectionPage',
+  10: '#setComplimentPage',
+  11: '#setSymmetricDifferencePage',
+  12: '#mergeSortPage',
+  13: '#bubbleSortPage',
+  14: '#binarySearchPage',
+  15: '#closestPairPage',
+  16: '#connectedGraphPage'
 }
 
-const makeChangePage = () => {
-  console.log('makeChange button works!')
+const hideAllPages = () => {
+  for(var i = 0; i <= Object.keys(routesObj).length; i++){
+    $(routesObj[i]).css({'display':'none'});
+  }
+}
+
+hideAllPages();
+$('#welcomePage').css({'display':'block'});
+
+const displayWelcomePage = () => {
+  hideAllPages();
+  $('#welcomePage').css({'display':'block'});
+  $(routesObj['makeChange']).css({'display':'none'});
+  console.log("routesObj['welcome']: ", JSON.stringify(routesObj['welcome']))
+}
+
+const displayMakeChangePage = () => {
+  hideAllPages();
+  $(routesObj['makeChange']).css({'display':'block'});
+  //$(routesObj['welcome']).css({'display':'none'});
+  console.log('makeChange page clicked')
 }
 
 const fizzBuzzPage = () => {
