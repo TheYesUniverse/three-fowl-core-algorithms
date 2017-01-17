9d52ab5054be2c7d23614f5ade81c3eea59c5904
mikeadossi <mikeadossi@gmail.com>
2017-01-16 21:34:04 -0800

    edit the description of your pages

--- spec/fibonacci.js
+++ spec/fibonacci.js
@@ -1,22 +1,7 @@
 "use strict"
-/*
-function fibonacci (number) {
-  if( typeof( number ) !== 'number' ) {
-    throw "InvalidInput"
-  }
-  let fibArray = [0,1]
-  if ( number < 3 )
-  return fibArray;
 
-  for ( let i = 2; i < number; i++ ) {
-    fibArray[i] = fibArray[i-1] + fibArray[i-2]
-  }
-  return fibArray;
-  }
-console.log(fibonacci(20))
-*/
-function fibonacci ( number ) {
-  var fibArray = [0, 1]
+const fibonacci = ( number ) => {
+  const fibArray = [0, 1]
   if ( number <= 0 ) return undefined;
   if ( number === 1 ) return fibArray = [0]
   if ( number === 2 ) return fibArray = [0,1]
