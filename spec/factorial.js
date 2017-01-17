7c8d5bfe0e2a61602a2780172fd0cddad52ec2db
mikeadossi <mikeadossi@gmail.com>
2017-01-16 21:10:51 -0800

    write factorial algorithm

---
+++ spec/factorial.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const factorial = (arg) => {
+
+       if(arg == 0) {
+           return 1;
+       }
+       if(arg < 0) {
+           return undefined;
+       }
+       for(var i = arg; --i; ) {
+           arg *= i;
+       }
+       return arg;
+}
+
+
+
+console.log('factorial of 5 is: ' + factorial(5))
